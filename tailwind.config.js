ff5d6bde0a76520360de3192628234db65655cf7
jennykut12 <jennykut.dj@gmail.com>
2024-04-19 17:33:08 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+// git remote add origin git@github.com:jennykut12/Github-Repo-Portfolio.git
+// git branch -M main
+// git push -u origin main
